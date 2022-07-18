6fce72c6d898fe317f35e26c6f416460ef7e6563
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-07-18 11:37:05 +0200

    make methods static

--- EncoreExtensionInterface.php
+++ EncoreExtensionInterface.php
@@ -5,12 +5,12 @@ namespace HeimrichHannot\EncoreContracts;
 interface EncoreExtensionInterface
 {
     /**
-     * @return string The bundle class
+     * @return string|class-string<\Symfony\Component\HttpKernel\Bundle\Bundle> The bundle class
      */
-    public function getBundle(): string;
+    public static function getBundle(): string;
 
     /**
      * @return array|EncoreEntry[]
      */
-    public function getEntries(): array;
+    public static function getEntries(): array;
 }
