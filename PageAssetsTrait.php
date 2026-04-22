ee58bc196d72c60d42ab2e6df2da271e4f823304
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-04-22 11:02:06 +0200

    use System service to avoid service contracts

--- PageAssetsTrait.php
+++ PageAssetsTrait.php
@@ -2,70 +2,29 @@
 
 namespace HeimrichHannot\EncoreContracts;
 
+use Contao\System;
 use HeimrichHannot\EncoreBundle\Asset\FrontendAsset;
-use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
-use Psr\Container\NotFoundExceptionInterface;
-use Symfony\Contracts\Service\Attribute\Required;
 
 trait PageAssetsTrait
 {
     use AddPageEntrypointTrait;
 
-    /** @var ContainerInterface $container */
-    protected $container;
-
-    public static function getSubscribedServices(): array
-    {
-        $services = \method_exists(\get_parent_class(self::class) ?: '', __FUNCTION__)
-            ? parent::getSubscribedServices()
-            : [];
-
-        if (\class_exists(FrontendAsset::class)) {
-            $services[] = '?'.FrontendAsset::class;
-        }
-
-        return $services;
-    }
-
-    #[Required]
-    public function setContainer(ContainerInterface $container): ?ContainerInterface
-    {
-        $ret = null;
-        if (\method_exists(\get_parent_class(self::class) ?: '', __FUNCTION__)) {
-            $ret = parent::setContainer($container);
-        }
-
-        $this->container = $container;
-
-        return $ret;
-    }
-
-    /**
-     * @return FrontendAsset|null
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
-    protected function getFrontendAsset(): mixed
+    protected function getFrontendAsset(): FrontendAsset|null
     {
         if (!\class_exists(FrontendAsset::class)) {
             return null;
         }
 
-        if (!isset($this->container)) {
-            return null;
+        if (isset($this->container) && $this->container instanceof ContainerInterface && $this->container->has(FrontendAsset::class)) {
+            return $this->container->get(FrontendAsset::class);
         }
 
-        if (!$this->container->has(FrontendAsset::class)) {
-            return null;
+        if (class_exists(System::class) && System::getContainer()->has(FrontendAsset::class)) {
+            return System::getContainer()->get(FrontendAsset::class);
+
         }
 
-        $service = $this->container->get(FrontendAsset::class);
-
-        if (!$service instanceof FrontendAsset) {
-            return null;
-        }
-
-        return $service;
+        return null;
     }
 }
