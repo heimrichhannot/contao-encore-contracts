afaaeba029e0b351bf2dc316599d16178e4323cd
Thomas <koertho@users.noreply.github.com>
2026-04-22 11:27:36 +0200

    Avoid service contracts (#6)

--- PageAssetsTrait.php
+++ PageAssetsTrait.php
@@ -2,19 +2,35 @@
 
 namespace HeimrichHannot\EncoreContracts;
 
+use Contao\System;
 use HeimrichHannot\EncoreBundle\Asset\FrontendAsset;
-use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
-use Psr\Container\NotFoundExceptionInterface;
-use Symfony\Contracts\Service\Attribute\Required;
 
 trait PageAssetsTrait
 {
     use AddPageEntrypointTrait;
 
-    /** @var ContainerInterface $container */
-    protected $container;
+    protected function getFrontendAsset(): mixed
+    {
+        if (!\class_exists(FrontendAsset::class)) {
+            return null;
+        }
 
+        if (isset($this->container) && $this->container instanceof ContainerInterface && $this->container->has(FrontendAsset::class)) {
+            return $this->container->get(FrontendAsset::class);
+        }
+
+        if (class_exists(System::class) && System::getContainer()->has(FrontendAsset::class)) {
+            return System::getContainer()->get(FrontendAsset::class);
+
+        }
+
+        return null;
+    }
+
+    /**
+     * Implement the ServiceSubscriberInterface method to not break existing usages.
+     */
     public static function getSubscribedServices(): array
     {
         $services = \method_exists(\get_parent_class(self::class) ?: '', __FUNCTION__)
@@ -26,46 +42,5 @@ trait PageAssetsTrait
         }
 
         return $services;
-    }
-
-    #[Required]
-    public function setContainer(ContainerInterface $container): ?ContainerInterface
-    {
-        $ret = null;
-        if (\method_exists(\get_parent_class(self::class) ?: '', __FUNCTION__)) {
-            $ret = parent::setContainer($container);
-        }
-
-        $this->container = $container;
-
-        return $ret;
-    }
-
-    /**
-     * @return FrontendAsset|null
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
-    protected function getFrontendAsset(): mixed
-    {
-        if (!\class_exists(FrontendAsset::class)) {
-            return null;
-        }
-
-        if (!isset($this->container)) {
-            return null;
-        }
-
-        if (!$this->container->has(FrontendAsset::class)) {
-            return null;
-        }
-
-        $service = $this->container->get(FrontendAsset::class);
-
-        if (!$service instanceof FrontendAsset) {
-            return null;
-        }
-
-        return $service;
     }
 }
